95057cab2ce57caf4a13bbe463d7493add4ad73f
newmanuevers <seangnewman@msn.com>
2018-12-09 14:08:04 -0700

    Completed Section 3

--- PluralSight_TricksAndTraps/Program.cs
+++ PluralSight_TricksAndTraps/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace PluralSight_TricksAndTraps
     {
         static void Main(string[] args)
         {
+            //DebuggerDisplayAttributeExample();
+            //DebuggerBrowsableAttributeExample();
+            //NullCoalescingOperatorExample();
+
+        }
+
+        private static void NullCoalescingOperatorExample()
+        {
+            NullCoalescingOperatorExample nullCoalescingOperatorExample = new NullCoalescingOperatorExample();
+            //nullCoalescingOperatorExample.ReferenceTypes();
+            nullCoalescingOperatorExample.NullableTypes();
+        }
+
+        private static void DebuggerBrowsableAttributeExample()
+        {
+            DebuggerBrowsableAttributeExample debuggerBrowsableAttributeExample = new DebuggerBrowsableAttributeExample();
+
+            //debuggerBrowsableAttributeExample.Without();
+            debuggerBrowsableAttributeExample.With();
+        }
+
+        private static void DebuggerDisplayAttributeExample()
+        {
+            DebuggerDisplayAttributeExample debuggerDisplayAttributeExample = new DebuggerDisplayAttributeExample();
+            //debuggerDisplayAttributeExample.Without();
+            debuggerDisplayAttributeExample.With();
         }
     }
 }
